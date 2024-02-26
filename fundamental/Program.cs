338fc5ba82539a34d99fb7dc117562f9690d4bf2
Yasmin <yka@luftborn.com>
2024-02-26 19:50:13 +0200

    declaration and assignment

--- fundamental/Program.cs
+++ fundamental/Program.cs
@@ -1,5 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.ReadKey();
+// declaration
+int num;
+//assignment 
+num = 5;
+//initialization
+int num2 = 10;
+
+//string refernce type
+string firstName = "Yasmin";
+string lastName = "Kasem";
+
+//regular concatanition  + sign
+string fullName=firstName + " " + lastName;
+Console.WriteLine(fullName);
+//string interpolation 
+
+
+
 
 
